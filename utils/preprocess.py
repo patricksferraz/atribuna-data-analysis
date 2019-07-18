6a7d5622dd78243b90867946554b831624c83169
patricksferraz <patrick.ferraz@outlook.com>
2019-07-18 14:18:41 -0300

    feat(preprocess): rename functions e add all2lower function

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -10,10 +10,26 @@ import unicodedata
 # In[5]:
 
 
-def _removes_nonascii(tokens):
+def _all2ascii(tokens):
     def _removes(token):
         token = unicodedata.normalize('NFKD', token)
         return token.encode('ascii', 'ignore').decode('utf-8', 'ignore')
-    
+
     return [_removes(_) for _ in tokens]
+
+
+# In[ ]:
+
+
+def _all2lower(tokens):
+    return [_.lower() for _ in tokens]
+
+
+# In[ ]:
+
+
+def normalize(tokens):
+    tokens = _all2ascii(tokens)
+    tokens = _all2lower(tokens)
+    return tokens
 
